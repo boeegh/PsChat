735ced22a22a52d15988ade67199f805210a55a8
suneb <3835239+suneb@users.noreply.github.com>
2023-12-15 11:42:07 +0100

    Updates readme with TTS and audio output in dual-minded example

--- examples/dual-minded.ps1
+++ examples/dual-minded.ps1
@@ -6,9 +6,10 @@
   Length of the conversation to generate.
 #>
 param(
-  [int]$ConversationLength = 10,
-  [string]$SystemPrompt = "Talk between a cop (Stan) and a robber (Peter). Prefix the characters role at the beginning of the reply.",
-  [string]$InitialQuestion = "Cop: Where were you yesterday at 8pm?"
+  [int]$ConversationLength = 3,
+  [string]$SystemPrompt = "Socratic style talk between a wise teacher and a young student. Stick to the topic of philosophy, leave out praise etc. between.",
+  [string]$InitialQuestion = "Student: What is the nature of reality?",
+  [switch]$SaveAudio
 )
 
 $messages = @(
@@ -16,17 +17,20 @@ $messages = @(
   @{ "Role"="user"; "Content"="$InitialQuestion" }
 )
 
-function Out-LastMessage {
-  $messages | Select-Object -Last 1 -ExpandProperty Content
-}
-
 for($i = 0; $i -lt $ConversationLength; $i++) {
-  Out-LastMessage
+  Write-Progress -Activity "Generating conversation" -Status "Playing it out $($i+1)/$ConversationLength" -PercentComplete (($i+1)/$ConversationLength*100)
   $messages = (Invoke-PsChat -Single -SkipQuestion -PreLoad_Objects $messages -NonInteractive -ResultType Objects)
 
   # reverse roles, making the assistant the user and vice versa
   foreach($message in $messages) {
+    if($message.Locked -eq $true) { continue }
     $message.Role = if($message.Role -eq "user") { "assistant" } else { "user" }
   }
 }
-Out-LastMessage
+
+$messages
+
+if($SaveAudio) {
+  # Write-Progress -Activity "Generating audio" -Status "Fetching and stiching audio" -PercentComplete 50
+  Invoke-PsChat -SaveAudio_Enabled $true -Single -NonInteractive -PreLoad_Objects $messages
+}
